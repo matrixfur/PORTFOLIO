e29b09e595c556c8fb74e3a6ab04a447515354a8
a.ansarhussain2004 <a.ansarhussain2004@gmail.com>
2024-12-13 16:00:57 +0000

    First commit

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "/home/user/portpholio/src/assets/profile-pic.png";
+import myImg from "../../assets/profile-pic.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
