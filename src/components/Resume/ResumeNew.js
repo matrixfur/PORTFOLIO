dd42dd6f35a7e602ac1e7bda2b6a60fc9655b45f
a.ansarhussain2004 <a.ansarhussain2004@gmail.com>
2024-12-13 15:55:47 +0000

    First commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "/home/user/portpholio/src/assets/ANSAR_HUSSAIN.pdf";
+import pdf from "../../assets/ANSAR_HUSSAIN.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
